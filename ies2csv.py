8620ec6d6f8efadf32d5ffa758c94a4c773eb8d5
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2018-08-08 21:53:05 -0700

    Begin logic for decoding IES

--- ies2csv.py
+++ ies2csv.py
@@ -11,6 +11,7 @@ Options:
     <path>                              Path for batch mode. [default: ./]
 """
 
+NULL_BYTE = '\x00'
 
 # from original code; may not be necessary
 def get_string(a_str, len: int):
@@ -27,10 +28,67 @@ def convert_file(file, dest=None):
         dest (str): the destination file name (default: None)
 
     Returns:
-        True
+        True if successful; False otherwise
     """
-    
     p = Path(file)
+
+    bstr = p.read_bytes()
+    table_name = bstr[0:128].decode().rstrip(NULL_BYTE)
+    valid = int.from_bytes(bstr[128:132], byteorder='little') # equivalent to original `val1`
+    offset1 = int.from_bytes(bstr[132:136], byteorder='little')
+    offset2 = int.from_bytes(bstr[136:140], byteorder='little')
+    filesize = int.from_bytes(bstr[140:144], byteorder='little')
+
+    if len(bstr) != filesize:
+        print('IES file has invalid length specified: ' + file)
+        return False
+    elif valid != 1:
+        print('IES file has incorrect value: ' + file)
+
+    # `short1` is unnecessary in this port
+    rows = int.from_bytes(bstr[146:148], byteorder='little')
+    cols = int.from_bytes(bstr[148:150], byteorder='little')
+    colint = int.from_bytes(bstr[150:152], byteorder='little')
+    colstr = int.from_bytes(bstr[140:144], byteorder='little')
+
+    if cols != colint + colstr:
+        print('IES file has mismatched cols: ' + file)
+        return False
+
+    offset_idx = filesize - offset1 - offset2
+
+    colnames = {}
+
+    for i in len(range(cols)):
+        new_offset = offset_idx
+        n1 = bstr[new_offset:new_offset+64]
+        new_offset += 64
+        n2 = bstr[new_offset:new_offset+64]
+        new_offset += 64
+        typ = bstr[new_offset:new_offset+2]
+        new_offset += 6 # 2 for short + 4 for `dummy`
+        # `dummy` is unnecessary in this port
+        pos = bstr[new_offset:new_offset+2]
+        new_offset += 2
+
+        if typ == 0:
+            try:
+                if colnames[pos] is not None:
+                    print('IES file is wrong: ' + file)
+                    return False
+            except KeyError:
+                pass
+            colnames[pos] = n1
+        else:
+            try:
+                if colnames[pos + colint] is not None:
+                    print('IES file is wrong: ' + file)
+                    return False
+            except KeyError:
+                pass
+            colnames[pos + colint] = n1
+        # Resume later; currently on line 75 of original
+
 
     out = Path(file if dest is None else dest)
     #out.write_text(txt)
